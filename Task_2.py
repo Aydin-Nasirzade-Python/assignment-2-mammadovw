b25c7c2f9e96e8621579a0c93ebc1d45461c2411
mammadovw <164747278+mammadovw@users.noreply.github.com>
2024-04-07 14:41:46 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -9,10 +9,10 @@ def main():
   elif ((ay=="June") and (gun>=21 and gun<=31)) or ((ay=="September" and gun<22) or (ay=="July" or ay=="August")):
       print(f"{ay} {gun} is in Summer")
       print()
-  elif ((ay=="September") and (gun>=22 and gun<=31)) or ((ay=="December" and gun<21) or (ay=="Octamber" or ay=="Novamber")):
+  elif ((ay=="September") and (gun>=22 and gun<=31)) or ((ay=="December" and gun<21) or (ay=="October" or ay=="November")):
       print(f"{ay} {gun} is in Fall")
       print()
-  elif ((ay=="December") and (gun>=21 and gun<=31)) or (ay=="March" and gun<20) or (ay=="January" or ay=="Febuary"):
+  elif ((ay=="December") and (gun>=21 and gun<=31)) or (ay=="March" and gun<20) or (ay=="January" or ay=="February"):
       print(f"{ay} {gun} is in Winter")
       print()
   pass
